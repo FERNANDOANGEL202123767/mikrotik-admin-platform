fe0ee8f7cc2e02e0579f7abe5373e5a764dbaeda
FERNANDOANGEL202123767 <fernandoangelhernandez62@gmail.com>
2025-06-10 22:07:08 -0600

    Update authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import { connectSocket, disconnectSocket } from './socketService';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://192.168.1.198:3000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
 
 export const login = async (credentials) => {
   try {
